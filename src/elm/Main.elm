09669cc9fcc029b8e6181add135c85b83d6ea1fb
Jason Leber <Jason Leber>
2017-07-18 11:30:40 -0500

    intial mockup

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,58 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (class, classList, type_)
+import Models exposing (..)
 
 
+main : Program Never Model Msg
 main =
-    text "Hello Electron. I'm Elm. Yay!? Reload??? Foreman!@#$#"
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
+
+
+
+-- Update
+
+
+type Msg
+    = None
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "layout" ]
+        [ div [ class "container" ]
+            [ h2 [] [ text "Search By Category" ]
+            , input [ class "search-box" ] []
+            ]
+        , div [ class "container" ]
+            [ h2 [] [ text "Search By Name" ]
+            , input [ class "search-box" ] []
+            ]
+        , node "hr" [] []
+        , div [ class "container" ]
+            [ h2 [] [ text "Upload a Profile" ]
+            , div [ class "row" ]
+                [ text "Category"
+                , select [ classList [ ( "search-box", True ), ( "select", True ) ] ]
+                    [ option [] [ text "Category A" ]
+                    , option [] [ text "Category B" ]
+                    , option [] [ text "Category N" ]
+                    ]
+                ]
+            , div [ class "row" ] [ input [ type_ "file", class "choose-file" ] [] ]
+            ]
+        ]
